c4ce363def6b20971f0d0f409df45405471be5cc
hilyas <hilyas@users.noreply.github.com>
2025-04-24 12:36:10 +0100

    Consolidate main.tf

--- main.tf
+++ main.tf
@@ -53,3 +53,46 @@ resource "launchdarkly_feature_flag" "new_checkout_experience" {
 
   tags = ["managed-by-terraform", "checkout", "ui"]
 }
+
+# Development environment flag settings
+resource "launchdarkly_feature_flag_environment" "new_checkout_dev" {
+  flag_id = launchdarkly_feature_flag.new_checkout_experience.id
+  env_key = "development"
+
+  on = true
+
+  # Always enabled in development
+  fallthrough {
+    variation = 0
+  }
+
+  off_variation = 1
+}
+
+# Production environment flag settings
+resource "launchdarkly_feature_flag_environment" "new_checkout_prod" {
+  flag_id = launchdarkly_feature_flag.new_checkout_experience.id
+  env_key = "production"
+
+  on = true
+
+  # Initially disabled in production
+  fallthrough {
+    variation = 1
+  }
+
+  # Beta testers get the feature enabled
+  rules {
+    description = "Beta testers"
+
+    clauses {
+      attribute = "email"
+      op        = "endsWith"
+      values    = ["@example.com"]
+    }
+
+    variation = 0
+  }
+
+  off_variation = 1
+}
